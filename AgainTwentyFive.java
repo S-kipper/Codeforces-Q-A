b406fdf8fcc8754c7e4df1aed8d95d8dc7da2fb7
S-kipper <adityasekhartiwariup@gmail.com>
2025-11-18 15:32:16 +0530

    Solved 630A

---
+++ AgainTwentyFive.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class AgainTwentyFive {
+
+    public static void main(String args[])  {
+    Scanner sc = new Scanner(System.in);
+    
+    double n = sc.nextDouble();
+    
+    //double res = Math.pow(5, n); Never in my life fuck HR
+    //int x = 25;
+    //int Result = (int)(res % 100);
+    System.out.print("25");//
+
+    sc.close();
+
+    }
+}
